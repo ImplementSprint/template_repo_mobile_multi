fb2f5bc28a50093e00edfda7c5e8d61d87545173
Hansysy <hansrocillo21@gmail.com>
2026-03-07 22:12:29 +0800

    Stabilize Kotlin tooling alignment for Android builds

--- scripts/detox-build-android.js
+++ scripts/detox-build-android.js
@@ -6,7 +6,7 @@ const isWindows = process.platform === 'win32';
 const gradleWrapper = isWindows ? 'gradlew.bat' : './gradlew';
 const androidDir = join(process.cwd(), 'android');
 const gradlePropertiesPath = join(androidDir, 'gradle.properties');
-const kotlinVersion = '2.0.21';
+const kotlinVersion = '2.1.20';
 
 function normalizeGradleProperties() {
   if (!existsSync(gradlePropertiesPath)) {
